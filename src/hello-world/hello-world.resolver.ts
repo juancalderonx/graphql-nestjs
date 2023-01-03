eae697357a4eb6cb968c2b1f80323a634d7496af
Esteban <juanescalderon12@gmail.com>
2023-01-03 13:16:36 -0500

    feat(query): :mag: Implementando más queries con GraphQL

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,11 +1,24 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query( () => String )
-    helloWorld(): string {
+    @Query( () => String, {
+        name: 'FunctionTest',
+        description: 'This is a test function that returns Hello World'
+    } )
+    hello(): string {
         return 'Hello World';
+    }
+
+    @Query( () => Float )
+    numberRandom(): number {
+        return Math.random() * 100;
+    }
+
+    @Query( () => Int )
+    getRandomFromZeroTo(): number {
+        return Math.round(Math.random() * 10);
     }
 
 }
